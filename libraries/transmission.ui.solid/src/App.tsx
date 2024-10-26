67fbcac134b81cf68acf7a6aea610162f4666c66
Aaron Westre <aaron@artificialnatures.com>
2024-10-26 16:10:17 -0500

    building and running in tauri

--- libraries/transmission.ui.solid/src/App.tsx
+++ libraries/transmission.ui.solid/src/App.tsx
@@ -4,11 +4,12 @@ const App: Component = () => {
   return (
     <div>
       <p class="text-5xl">
-        Here's some words -&gt; some words
+        Here's some words =&gt; some words
       </p>
       <p class="text-5xl">
-        Here's some numbers -&gt; 1234567890
+        Here's some numbers =&gt; 1234567890
       </p>
+      <button class="btn btn-accent">Ok!</button>
     </div>
   );
 };
